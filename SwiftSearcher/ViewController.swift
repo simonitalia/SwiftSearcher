65c717f118614d36ea369972b3c81903b97dc1fd
Simon Italia <sitalia@magicaltomato.com>
2019-04-09 14:15:54 +0800

    Initial Commit

---
+++ SwiftSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSearcher
+//
+//  Created by Simon Italia on 4/9/19.
+//  Copyright © 2019 SDI Group Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
